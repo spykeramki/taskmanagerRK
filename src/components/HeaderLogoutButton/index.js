e48862a8419bb9a61eec1ed9a4c4c5b88118d95f
phani murari <imphanimurari1@gmail.com>
2022-04-22 16:57:56 +0000

    added to CCBP IDE

--- src/components/HeaderLogoutButton/index.js
+++ src/components/HeaderLogoutButton/index.js
@@ -1,11 +1,22 @@
-import './index.css'
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import "./index.css";
 
-const HeaderLogoutButton = () => {
-    return(
-        <button type="button" className="header-logout-button">
-          Log Out
-        </button>
-    )
-}
+const HeaderLogoutButton = (props) => {
+  const navigate = useNavigate();
+  const onClickLogout = () => {
+    Cookies.remove("trello_access_token");
+    navigate("/login");
+  };
+  return (
+    <button
+      type="button"
+      className="header-logout-button"
+      onClick={onClickLogout}
+    >
+      Log Out
+    </button>
+  );
+};
 
-export default HeaderLogoutButton
+export default HeaderLogoutButton;
